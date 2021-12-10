8b81b3856d6e968578d08509e626391f67a05dc3
n0xZ <o.gonzalo@hotmail.com>
2021-12-10 19:17:46 -0300

    add project component route

---
+++ pages/projects.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProjectForm from '@/components/Form/ProjectForm';
+const ProjectsFormView = () => {
+    return <ProjectForm />;
+};
+
+export default ProjectsFormView;
