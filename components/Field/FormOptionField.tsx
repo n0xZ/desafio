b704ef451c7c29eece4417157c08ec94d9b9494c
n0xZ <o.gonzalo@hotmail.com>
2021-12-10 19:16:10 -0300

    add form option field with functionality and styles

---
+++ components/Field/FormOptionField.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FieldError, UseFormRegister } from 'react-hook-form';
+type Persons={
+    name:string
+}
+interface IFormOptionField {
+    labelName: string;
+    inputName: string;
+    register: UseFormRegister<any>;
+    errors: FieldError | undefined;
+    options: string[] | Persons[];
+}
+const FormOptionField: React.FC<IFormOptionField> = ({
+    inputName,
+    labelName,
+    register,
+    errors,
+    options,
+}) => {
+    return (
+        <main className="flex flex-col mb-4 w-full  ">
+            <label className="font-roboto">{labelName}</label>
+            <select
+                className="h-11 w-full rounded-md outline-none border-2 border-gray-300 shadow-sm"
+                {...register(inputName)}
+            >
+                {options.map((option, index) => (
+                    <option key={index} value={option.name}>
+                        {option.name}
+                    </option>
+                ))}
+            </select>
+            <p className="text-red-600 ">{errors?.message && errors.message}</p>
+        </main>
+    );
+};
+
+export default FormOptionField;
