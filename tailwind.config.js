1f654631464e76ba38240f22c1527ea791477754
n0xZ <o.gonzalo@hotmail.com>
2021-12-10 19:17:31 -0300

    add purge configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,27 @@
 module.exports = {
-    content: [],
+    purge: {
+        enabled: true,
+        content: ['./pages/**/*.tsx', './components/**/*.tsx'],
+        options: {
+            safelist: ['dark'],
+        },
+    },
+    darkMode: 'class', // or 'media' or 'class'
     theme: {
         extend: {
             fontFamily: {
-                roboto: 'Roboto',
+                roboto: 'Roboto ',
                 ubuntu: 'Ubuntu',
+     
+            },
+            colors: {
+                mint: '#63FFC7',
+                darkPurple: '#24243e',
             },
         },
+    },
+    variants: {
+        extend: {},
     },
     plugins: [],
 };
