0d2e55866fb4e3f6dad8c7f463d318815e110d00
n0xZ <o.gonzalo@hotmail.com>
2021-12-13 09:55:52 -0300

    set custom headers to every api route

--- next.config.js
+++ next.config.js
@@ -4,4 +4,26 @@ module.exports = {
     env: {
         API_URL: process.env.NEXT_APP_API_URL,
     },
+    async headers() {
+        return [
+            {
+                source: '/api/projects',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                ],
+            },
+            {
+                source: '/api/projects/:id',
+                headers: [
+                    {
+                        key: 'Access-Control-Allow-Origin',
+                        value: '*',
+                    },
+                ],
+            },
+        ];
+    },
 };
