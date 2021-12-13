2ceda5c884dc12a9f26f89d333e67411c3f25b81
n0xZ <o.gonzalo@hotmail.com>
2021-12-13 10:55:50 -0300

    fix card styles and pagination

--- components/Projects/ProjectsList.tsx
+++ components/Projects/ProjectsList.tsx
@@ -49,7 +49,8 @@ const ProjectsList = ({ projects }: IProjectsList) => {
         settoggleMenu((prev) => (prev = !prev));
     };
     const NextPage = () => {
-        if (ItemsPerPage <= Projects.length) setItemsPerPage((prev) => prev + 5);
+        if (ItemsPerPage+5 <= Projects.length)
+            setItemsPerPage((prev) => prev + 5);
     };
     const PreviousPage = () => {
         if (ItemsPerPage > 0) setItemsPerPage((prev) => prev - 5);
@@ -57,63 +58,73 @@ const ProjectsList = ({ projects }: IProjectsList) => {
 
     return (
         <div className="flex flex-col justify-between min-h-screen w-full">
-            {SlicedProjects().map((project) => (
-                <div
-                    key={project.id}
-                    className="flex flex-row justify-between items-center  max-h-2/6 w-full p-3 bg-white border-gray-400 border-b-2  "
-                >
-                    <aside className="h-full w-full flex-col justify-start items-start ">
-                        <h3 className="font-roboto font-bold">
-                            {project.name}
-                        </h3>
-                        <p className="text-gray-500 text-xs font-roboto mx-1 mb-2">
-                            Created at {project.createdAt}
-                        </p>
-                        <h2 className="font-roboto font-bold mx-6 h-full">
-                            {project.projectManager}
-                        </h2>
-                    </aside>
-                    <div className="flex flex-row mx-3 justify-end h-full w-full items-center">
-                        <div
-                            key={project.projectManager}
-                            className={
-                                toggleMenu && SelectedProject === project.id
-                                    ? ' h-full xl:w-4/12 mx-4 w-2/4 rounded-md shadow-md flex flex-col justify-center items-center  bg-gray-50'
-                                    : ' hidden '
-                            }
-                        >
-                            <div className="text-left  w-full flex flex-row  justify-start items-center">
-                                <button
-                                    onClick={() => DeleteProject(project.id)}
-                                    className="text-left  border-b-2 border-gray-200 w-full flex flex-row justify-start items-center"
-                                >
-                                    <RiDeleteBin7Line className="xl:h-6 xl:w-6  w-16" />{' '}
-                                    <p className="mx-2">Eliminar</p>
-                                </button>
+            <div>
+                {SlicedProjects().map((project) => (
+                    <div
+                        key={project.id}
+                        className="flex flex-row justify-start items-center  min-h-2/6 w-full p-3 bg-white border-gray-400 border-b-2  "
+                    >
+                        <aside className="h-full w-full flex-col justify-start items-start ">
+                            <h3 className="font-roboto font-bold">
+                                {project.name}
+                            </h3>
+                            <p className="text-gray-500 text-xs font-roboto mx-1 mb-2">
+                                Created at {project.createdAt}
+                            </p>
+                            <h2 className="font-roboto font-bold mx-6 h-full">
+                                {project.projectManager}
+                            </h2>
+                        </aside>
+                        <div className="flex flex-row mx-3 justify-end h-full w-full items-center">
+                            <div
+                                key={project.projectManager}
+                                className={
+                                    toggleMenu && SelectedProject === project.id
+                                        ? ' h-full xl:w-4/12 mx-4 w-2/4 rounded-md shadow-md flex flex-col justify-center items-center  bg-gray-50'
+                                        : ' hidden '
+                                }
+                            >
+                                <div className="text-left  w-full flex flex-row  justify-start items-center">
+                                    <button
+                                        onClick={() =>
+                                            DeleteProject(project.id)
+                                        }
+                                        className="text-left  border-b-2 border-gray-200 w-full flex flex-row justify-start items-center"
+                                    >
+                                        <RiDeleteBin7Line className="xl:h-6 xl:w-6  w-16" />{' '}
+                                        <p className="mx-2">Eliminar</p>
+                                    </button>
+                                </div>
+                                <div className="text-left  w-full flex flex-row ml-2 justify-start items-center">
+                                    <Link href={`/edit/${project.id}`}>
+                                        <a className="flex flex-row justify-start items-center">
+                                            <FiEdit className="xl:h-5 xl:w-5 h-4 w-4" />{' '}
+                                            <p className="mx-2">Editar</p>
+                                        </a>
+                                    </Link>
+                                </div>
                             </div>
-                            <div className="text-left  w-full flex flex-row ml-2 justify-start items-center">
-                                <Link href={`/edit/${project.id}`}>
-                                    <a className="flex flex-row justify-start items-center">
-                                        <FiEdit className="xl:h-5 xl:w-5 h-4 w-4" />{' '}
-                                        <p className="mx-2">Editar</p>
-                                    </a>
-                                </Link>
-                            </div>
+                            <GoKebabVertical
+                                className="h-2/5"
+                                onClick={() => ToggleMenu(project.id)}
+                            />
                         </div>
-                        <GoKebabVertical
-                            className="h-2/5"
-                            onClick={() => ToggleMenu(project.id)}
-                        />
                     </div>
-                </div>
-            ))}
+                ))}
+            </div>
             <div className="flex flex-col bg-red justify-center items-center w-full h-full mt-5">
                 <div className="flex flex-row items-center ">
-                    <button className="px-3 py-2 mx-6 rounded bg-red-600  font-roboto  text-white hover:bg-red-800 transition ease-in " onClick={PreviousPage}>
+                    <button
+                        className="px-3 py-2 mx-6 rounded bg-red-600  font-roboto  text-white hover:bg-red-800 transition ease-in "
+                        onClick={PreviousPage}
+                    >
                         Previous
                     </button>
 
-                    <button className="px-6 py-2 mx-2 rounded bg-blue-600  font-roboto  text-white hover:bg-blue-800 transition ease-in " onClick={NextPage}>
+                    <button
+                        className="px-6 py-2 mx-2 rounded bg-blue-600  font-roboto  text-white hover:bg-blue-800 transition ease-in "
+                        onClick={NextPage}
+                    >
                         Next
                     </button>
                 </div>
