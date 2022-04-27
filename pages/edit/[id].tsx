a696ba39dfc3afff844eb6ee8e03f2315c10cf86
Gonzalo Molina <o.gonzalo@hotmail.com>
2022-04-27 16:41:42 -0300

    add SSR to every page that needs to have full-time data related to Projects

--- pages/edit/[id].tsx
+++ pages/edit/[id].tsx
@@ -1,54 +1,55 @@
-import React from 'react';
+import {
+	GetServerSideProps,
+	InferGetServerSidePropsType,
+	NextPage,
+} from 'next';
 import Head from 'next/head';
 import ProjectForm from '@/components/Form/ProjectForm';
 
-import { GetStaticPaths, GetStaticProps } from 'next';
 import { Project } from 'types';
-interface IProject {
-    project: Project;
-}
-const Project: React.FC<IProject> = ({ project }) => {
-    return (
-        <>
-            <Head>
-                <title>Challenge Esto Es | Editar Proyecto</title>
-                <meta
-                    name="Challenge Esto Es "
-                    content="Formulario de edición de proyecto de la página Esto Es"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-            <ProjectForm isEditAvailable={true} projectId={project.id} />
-        </>
-    );
+
+const Project: NextPage = ({
+	project,
+}: InferGetServerSidePropsType<
+	typeof getServerSideProps
+>) => {
+	return (
+		<>
+			<Head>
+				<title>Challenge Esto Es | Editar Proyecto</title>
+				<meta
+					name="Challenge Esto Es "
+					content="Formulario de edición de proyecto de la página Esto Es"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<ProjectForm
+				isEditAvailable={true}
+				projectId={project.id}
+			/>
+		</>
+	);
 };
 
 export default Project;
 
-export const getStaticProps: GetStaticProps<any, any> = async ({
-    params: { id },
-}) => {
-    const API_URL = String(process.env.API_URL);
-    const projects = await fetch(API_URL);
-    const projectResponse = await projects.json();
-    const project = projectResponse.find(
-        (project: Project) => project.id === id
-    );
-    return {
-        props: {
-            project,
-        },
-        revalidate: 1,
-    };
-};
-export const getStaticPaths: GetStaticPaths = async () => {
-    const API_URL = String(process.env.API_URL);
-    const projects = await fetch(API_URL);
-    const projectResponse = await projects.json();
-    return {
-        paths: projectResponse.map((project: Project) => ({
-            params: { id: project.id },
-        })),
-        fallback: 'blocking',
-    };
+export const getServerSideProps: GetServerSideProps<
+	any,
+	any
+> = async ({ params: { id } }) => {
+	const API_URL = String(process.env.API_URL);
+	const projects = await fetch(API_URL);
+	const projectResponse = await projects.json();
+	const project = projectResponse.find(
+		(project: Project) => project.id === id
+	);
+	if (!project)
+		return {
+			redirect: { destination: '/error', permanent: false },
+		};
+	return {
+		props: {
+			project,
+		},
+	};
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,50 @@
-import type { GetServerSideProps, GetStaticProps } from 'next';
+import type {
+	GetServerSideProps,
+	InferGetServerSidePropsType,
+	NextPage,
+} from 'next';
 import Head from 'next/head';
 
 import HomeView from '@/components/Home/HomeView';
 import { Project } from 'types';
 import Header from '@/components/Header/Header';
-interface IProjects {
-    projects: Project[];
-}
-const Home: React.FC<IProjects> = ({ projects }) => {
-    return (
-        <div className="min-h-screen h-full w-full">
-            <Head>
-                <title>Challenge Esto Es | Home</title>
-                <meta
-                    name="Challenge Esto Es "
-                    content="Pagina principal de Challenge Esto Es"
-                />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
 
-            <Header />
-            <div className="mb-4">
-                <div>
-                    <HomeView projects={projects} />
-                </div>
-            </div>
-        </div>
-    );
+const Home: NextPage = ({
+	projects,
+}: InferGetServerSidePropsType<
+	typeof getServerSideProps
+>) => {
+	return (
+		<div className="min-h-screen h-full w-full">
+			<Head>
+				<title>Challenge Esto Es | Home</title>
+				<meta
+					name="Challenge Esto Es "
+					content="Pagina principal de Challenge Esto Es"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<Header />
+			<div className="mb-4">
+				<div>
+					<HomeView projects={projects} />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home;
-export const getStaticProps: GetStaticProps = async () => {
-    const API_URL = String(process.env.API_URL);
-    const projectsResponse = await fetch(API_URL);
-    const projects = await projectsResponse.json();
-    return {
-        props: {
-            projects: projects,
-        },
-        revalidate:1
-    };
-};
+export const getServerSideProps: GetServerSideProps =
+	async () => {
+		const API_URL = String(process.env.API_URL);
+		const projectsResponse = await fetch(API_URL);
+		const projects = await projectsResponse.json();
+		console.log(projects);
+		return {
+			props: {
+				projects: projects,
+			},
+		};
+	};
